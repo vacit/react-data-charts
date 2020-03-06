3accdf9b9e0a0f6a78ce15c2edd3b0faa8c764b6
vacit <vacitabdulrazak@gmail.com>
2020-03-06 04:36:33 +0300

    Updates

---
+++ precache-manifest.ea27fdbb119662bebc13d5160f7db703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8a6bdb46ad2a1b03e052e1f3773abe",
+    "url": "/react-data-charts/index.html"
+  },
+  {
+    "revision": "7347e7f5f3b69a12e859",
+    "url": "/react-data-charts/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d21e57bbb296e982d29f",
+    "url": "/react-data-charts/static/js/2.3f272596.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-data-charts/static/js/2.3f272596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7347e7f5f3b69a12e859",
+    "url": "/react-data-charts/static/js/main.9178ae65.chunk.js"
+  },
+  {
+    "revision": "2f942d0afa9e5c0f3cf7",
+    "url": "/react-data-charts/static/js/runtime-main.0f16f49f.js"
+  }
+]);
